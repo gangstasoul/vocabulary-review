b09867161b957651969050f3f2ad8f3d092a4973
gangstasoul <30682890+gangstasoul@users.noreply.github.com>
2024-10-12 17:47:36 +0700

    Add files via upload

---
+++ files/library-player-1.js
@@ -0,0 +1,257 @@
+var word1 = document.createElement('img');word1.src = './animals-media/amphibians.png';word1.height = 150;
+var true1 = 'amphibians';
+var false1 = 'mammals';
+var false1_2 = 'predators';
+var mean1; 
+var word2 = document.createElement('img');word2.src = './animals-media/antlers.png';word2.height = 150;
+var true2 = 'antlers';
+var false2 = 'horns';
+var false2_2 = 'antennas';
+var mean2; 
+var word3 = document.createElement('img');word3.src = './animals-media/beak.png';word3.height = 150;
+var true3 = 'beak';
+var false3 = 'mouth';
+var false3_2 = 'snout';
+var mean3; 
+var word4 = document.createElement('img');word4.src = './animals-media/birds.png';word4.height = 150;
+var true4 = 'birds';
+var false4 = 'fish';
+var false4_2 = 'plants';
+var mean4; 
+var word5 = document.createElement('img');word5.src = './animals-media/bony_plates.png';word5.height = 150;
+var true5 = 'bony plates';
+var false5 = 'bulletproof vest';
+var false5_2 = 'leather scales';
+var mean5; 
+var word6 = document.createElement('img');word6.src = './animals-media/claws.png';word6.height = 150;
+var true6 = 'claws';
+var false6 = 'fangs';
+var false6_2 = 'talons';
+var mean6; 
+var word7 = document.createElement('img');word7.src = './animals-media/domestic_animals.png';word7.height = 150;
+var true7 = 'domestic animals';
+var false7 = 'wild animals';
+var false7_2 = 'human species';
+var mean7; 
+var word8 = document.createElement('img');word8.src = './animals-media/endangered_species.png';word8.height = 150;
+var true8 = 'endangered species';
+var false8 = 'pets';
+var false8_2 = 'livestock';
+var mean8; 
+var word9 = document.createElement('img');word9.src = './animals-media/fangs.png';word9.height = 150;
+var true9 = "fangs";
+var false9 = 'claws';
+var false9_2 = 'teeth';
+var mean9; 
+var word10 = document.createElement('img');word10.src = './animals-media/feathers.png';word10.height = 150;
+var true10 = "feathers";
+var false10 = 'skin';
+var false10_2 = 'spikes';
+var mean10;
+ 
+var word11 = document.createElement('img');word11.src = './animals-media/fin.png';word11.height = 150;
+var true11 = "fin";
+var false11 = "web";
+var false11_2 = 'gills';
+var mean11; 
+var word12 = document.createElement('img');word12.src = './animals-media/fish.png';word12.height = 150;
+var true12 = "fish";
+var false12 = "amphibians";
+var false12_2 = 'reptiles';
+var mean12; 
+var word13 = document.createElement('img');word13.src = './animals-media/fur.png';word13.height = 150;
+var true13 = "fur";
+var false13 = "skin";
+var false13_2 = 'hair';
+var mean13; 
+var word14 = document.createElement('img');word14.src = './animals-media/gills.png';word14.height = 150;
+var true14 = "gills";
+var false14 = "fins";
+var false14_2 = 'lungs';
+var mean14; 
+var word15 = document.createElement('img');word15.src = './animals-media/hooves.png';word15.height = 150;
+var true15 = "hooves";
+var false15 = "feet";
+var false15_2 = "claws";
+var mean15;
+var word16 = document.createElement('img');word16.src = './animals-media/horns.png';word16.height = 150;
+var true16 = "horns";
+var false16 = "antlers";
+var false16_2 = "antennas";
+var mean16;
+var word17 = document.createElement('img');word17.src = './animals-media/hump.png';word17.height = 150;
+var true17 = "hump";
+var false17 = "hill";
+var false17_2 = "belly";
+var mean17;
+var word18 = document.createElement('img');word18.src = './animals-media/insects.png';word18.height = 150;
+var true18 = "insects";
+var false18 = "bugs";
+var false18_2 = "beetles";
+var mean18; 
+var word19 = document.createElement('img');word19.src = './animals-media/limbs.png';word19.height = 150;
+var true19 = "limbs";
+var false19 = "wings";
+var false19_2 = "arms";
+var mean19; 
+var word20 = document.createElement('img');word20.src = './animals-media/lizard.png';word20.height = 150;
+var true20 = "lizard";
+var false20 = "frog";
+var false20_2 = "snake";
+var mean20; 
+var word21 = document.createElement('img');word21.src = './animals-media/mammals.png';word21.height = 150;
+var true21 = "mammals";
+var false21 = "herbivores";
+var false21_2 = "predators";
+var mean21; 
+var word22 = document.createElement('img');word22.src = './animals-media/mane.png';word22.height = 150;
+var true22 = "mane";
+var false22 = "hair";
+var false22_2 = "paw";
+var mean22; 
+var word23 = document.createElement('img');word23.src = './animals-media/paws.png';word23.height = 150;
+var true23 = "paws";
+var false23 = "legs";
+var false23_2 = "arms";
+var mean23; 
+var word24 = document.createElement('img');word24.src = './animals-media/pouch.png';word24.height = 150;
+var true24 = "pouch";
+var false24 = "bag";
+var false24_2 = "pocket";
+var mean24; 
+var word25 = document.createElement('img');word25.src = './animals-media/reptiles.png';word25.height = 150;
+var true25 = "reptiles";
+var false25 = "mammals";
+var false25_2 = "amphibians";
+var mean25; 
+var word26 = document.createElement('img');word26.src = './animals-media/rodents.png';word26.height = 150;
+var true26 = "rodents";
+var false26 = "feline";
+var false26_2 = "canine";
+var mean26; 
+var word27 = document.createElement('img');word27.src = './animals-media/scales.png';word27.height = 150;
+var true27 = "scales";
+var false27 = "skin";
+var false27_2 = "leather";
+var mean27; 
+var word28 = document.createElement('img');word28.src = './animals-media/shell.png';word28.height = 150;
+var true28 = "shell";
+var false28 = "skin";
+var false28_2 = "bone";
+var mean28; 
+var word29 = document.createElement('img');word29.src = './animals-media/snout.png';word29.height = 150; 
+var true29 = "snout";
+var false29 = "nose";
+var false29_2 = "mouth";
+var mean29;
+var word30 = document.createElement('img');word30.src = './animals-media/tail.png';word30.height = 150;
+var true30 = "tail";
+var false30 = "trunk";
+var false30_2 = "fin";
+var mean30; 
+var word31 = document.createElement('img');word31.src = './animals-media/talons.png';word31.height = 150;
+var true31 = "talons";
+var false31 = "fangs";
+var false31_2 = "nails";
+var mean31; 
+var word32 = document.createElement('img');word32.src = './animals-media/tentacles.png';word32.height = 150;
+var true32 = "tentacles";
+var false32 = "arms";
+var false32_2 = "legs";
+var mean32; 
+var word33 = document.createElement('img');word33.src = './animals-media/trunk.png';word33.height = 150;
+var true33 = "trunk";
+var false33 = "nose";
+var false33_2 = "snout";
+var mean33; 
+var word34 = document.createElement('img');word34.src = './animals-media/tusks.png';word34.height = 150;
+var true34 = "tusks";
+var false34 = "teeth";
+var false34_2 = "fangs";
+var mean34; 
+var word35 = document.createElement('img');word35.src = './animals-media/web.png';word35.height = 150;
+var true35 = "web";
+var false35 = "fin";
+var false35_2 = "paw";
+var mean35; 
+var word36 = document.createElement('img');word36.src = './animals-media/whiskers.png';word36.height = 150;
+var true36 = "whiskers";
+var false36 = "hair";
+var false36_2 = "antennas";
+var mean36;
+var word37 = document.createElement('img');word37.src = './animals-media/wild_animals.png';word37.height = 150; 
+var true37 = "wild animals";
+var false37 = "domestic animals";
+var false37_2 = "pets";
+var mean37;
+var word38 = document.createElement('img');word38.src = './animals-media/wing.png';word38.height = 150;
+var true38 = "wing";
+var false38 = "arm";
+var false38_2 = "leg";
+var mean38; 
+var word39 = document.createElement('img');word39.src = './animals-media/predators.png';word39.height = 150;
+var true39 = "predators";
+var false39 = "herbivores";
+var false39_2 = "domestic animals";
+var mean39; 
+var word40 = document.createElement('img');word40.src = './animals-media/herbivore.png';word40.height = 150;
+var true40 = "herbivore";
+var false40 = "predator";
+var false40_2 = "wild animal";
+var mean40; 
+
+
+    var error = "error!!!";
+	var puzzle_sprint_words = 
+	{
+	"0":[
+	{"id":1,"diff":0,"word":word1,"visible_translation":error,"visible_translation2":error,"visible_translation3":error,"translation":true1,"fake_translation":false1,"fake_translation2":false1_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"adjective"},
+	{"id":2,"diff":0,"word":word2,"visible_translation":error,"visible_translation2":error,"visible_translation3":error,"translation":true2,"fake_translation":false2,"fake_translation2":false2_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":3,"diff":0,"word":word3,"visible_translation":error,"visible_translation2":error,"visible_translation3":error,"translation":true3,"fake_translation":false3,"fake_translation2":false3_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"adjective"},
+	{"id":4,"diff":0,"word":word4,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true4,"fake_translation":false4,"fake_translation2":false4_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"adjective"},
+	{"id":5,"diff":0,"word":word5,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true5,"fake_translation":false5,"fake_translation2":false5_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":6,"diff":0,"word":word6,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true6,"fake_translation":false6,"fake_translation2":false6_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":7,"diff":0,"word":word7,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true7,"fake_translation":false7,"fake_translation2":false7_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":8,"diff":0,"word":word8,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true8,"fake_translation":false8,"fake_translation2":false8_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":9,"diff":0,"word":word9,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true9,"fake_translation":false9,"fake_translation2":false9_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"pronoun"},
+	{"id":10,"diff":0,"word":word10,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true10,"fake_translation":false10,"fake_translation2":false10_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"adverb"},
+	{"id":11,"diff":0,"word":word11,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true11,"fake_translation": false11,"fake_translation2":false11_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"adjective"},
+	{"id":12,"diff":0,"word":word12,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true12,"fake_translation":false12,"fake_translation2":false12_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":13,"diff":0,"word":word13,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true13,"fake_translation":false13,"fake_translation2":false13_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"adjective"},
+	{"id":14,"diff":0,"word":word14,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true14,"fake_translation":false14,"fake_translation2":false14_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"adjective"},
+	{"id":15,"diff":0,"word":word15,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true15,"fake_translation":false15,"fake_translation2":false15_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":16,"diff":0,"word":word16,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true16,"fake_translation":false16,"fake_translation2":false16_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":17,"diff":0,"word":word17,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true17,"fake_translation":false17,"fake_translation2":false17_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":18,"diff":0,"word":word18,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true18,"fake_translation":false18,"fake_translation2":false18_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":19,"diff":0,"word":word19,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true19,"fake_translation":false19,"fake_translation2":false19_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"pronoun"},
+	{"id":20,"diff":0,"word":word20,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true20,"fake_translation":false20,"fake_translation2":false20_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"adverb"},
+	{"id":21,"diff":0,"word":word21,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true21,"fake_translation":false21,"fake_translation2":false21_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":22,"diff":0,"word":word22,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true22,"fake_translation":false22,"fake_translation2":false22_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":23,"diff":0,"word":word23,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true23,"fake_translation":false23,"fake_translation2":false23_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":24,"diff":0,"word":word24,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true24,"fake_translation":false24,"fake_translation2":false24_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":25,"diff":0,"word":word25,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true25,"fake_translation":false25,"fake_translation2":false25_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":26,"diff":0,"word":word26,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true26,"fake_translation":false26,"fake_translation2":false26_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":27,"diff":0,"word":word27,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true27,"fake_translation":false27,"fake_translation2":false27_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":28,"diff":0,"word":word28,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true28,"fake_translation":false28,"fake_translation2":false28_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":29,"diff":0,"word":word29,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true29,"fake_translation":false29,"fake_translation2":false29_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":30,"diff":0,"word":word30,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true30,"fake_translation":false30,"fake_translation2":false30_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"adjective"},
+	{"id":31,"diff":0,"word":word31,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true31,"fake_translation":false31,"fake_translation2":false31_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"adjective"},
+	{"id":32,"diff":0,"word":word32,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true32,"fake_translation":false32,"fake_translation2":false32_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"adjective"},
+	{"id":33,"diff":0,"word":word33,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true33,"fake_translation":false33,"fake_translation2":false33_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"adjective"},
+	{"id":34,"diff":0,"word":word34,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true34,"fake_translation":false34,"fake_translation2":false34_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":35,"diff":0,"word":word35,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true35,"fake_translation":false35,"fake_translation2":false35_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"adjective"},
+	{"id":36,"diff":0,"word":word36,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true36,"fake_translation":false36,"fake_translation2":false36_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"adjective"},
+	{"id":37,"diff":0,"word":word37,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true37,"fake_translation":false37,"fake_translation2":false37_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":38,"diff":0,"word":word38,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true38,"fake_translation":false38,"fake_translation2":false38_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":39,"diff":0,"word":word39,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true39,"fake_translation":false39,"fake_translation2":false39_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+	{"id":40,"diff":0,"word":word40,"visible_translation":error,"visible_translation2": error,"visible_translation3": error,"translation":true40,"fake_translation":false40,"fake_translation2":false40_2,"correct":error,"shown":0,"speakers":null,"part_of_speech":"noun"},
+					
+	],	
+		};
+
+   var in_rating = '1';
+    var adjust_level = '0';
+	
+	
+
+	
